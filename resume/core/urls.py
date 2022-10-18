9afa44f85221aa339af3433dedeaafd7cc67e139
ana-gujabidze <ana.gujabidze@quantori.com>
2022-10-18 21:34:55 +0400

    Configure URLs

--- resume/core/urls.py
+++ resume/core/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path, re_path
+from resume_builder import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    re_path("admin/", admin.site.urls),
+    re_path("tinymce/", include("tinymce.urls")),
+    re_path(r"^resume/", include("resume_builder.urls")),
+    re_path(r"^$", views.HomepageView.as_view(), name="homepage"),
 ]
+
+# Serving the media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
+
+handler500 = "utils.views.error_500"
+handler404 = "utils.views.error_404"
