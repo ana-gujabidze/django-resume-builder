2e1cb1c24e7b9185410a9199042a9c6007c6d670
ana-gujabidze <ana.gujabidze@quantori.com>
2022-10-18 21:22:38 +0400

    Move SECRET_KEY to .env

--- resume/core/settings.py
+++ resume/core/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_env
+
+load_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=*=5$=$act(oinb%kffqi++go#_=2qcldp!%5+lg&wez_gc409"
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
